197dd09c3aec15ac813b90bae1783c8cd4342586
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-03-01 20:08:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,4 +22,20 @@ async def on_ready():
 async def on_member_join(member):
     await message.channel.send(f"{member}さんいらっしゃい！")
 
+@client.event
+async def on_message(message):
+
+    url_re = r"https://discordapp.com/channels/(\d{18})/(\d{18})/(\d{18})"
+    url_list  = re.findall(url_re,message.content)
+    
+    for url in url_list:
+        guild_id,channel_id,message_id = url
+        channel = client.get_channel(int(channel_id))
+
+        if channel is not None:
+            got_message = await channel.fetch_message(message_id)
+
+            if got_message is not None:
+                await message.channel.send(embed=open_message(got_message))
+ 
 client.run(token)
